0981493b9d098dac85212bccc06777914e12a8be
Thhh <thyagonovicki@gmail.com>
2025-12-30 08:44:11 -0300

    Update AndroidConfig with versioning and libs
    Added versioning and library dependencies to AndroidConfig.

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -1,6 +1,13 @@
 object AndroidConfig {
     const val compileSdk = 34
     const val minSdk = 21
-    @Suppress("UNUSED")
     const val targetSdk = 34
+
+    const val versionCode = 1
+    const val versionName = "1.0"
+
+    object Libs {
+        const val injekt = "com.github.inorichi.injekt:injekt-core:0.16.1"
+        const val extensionsLib = "com.github.tachiyomiorg:extensions-lib:1.4.3"
+    }
 }
